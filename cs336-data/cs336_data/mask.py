5bf7f0c94803f95566ddffebf580cb1fa13f978e
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2024-05-19 14:07:30 -0700

    google phonenumbers

--- cs336-data/cs336_data/mask.py
+++ cs336-data/cs336_data/mask.py
@@ -1,10 +1,13 @@
 import re
+import phonenumbers
+from phonenumbers import PhoneNumberMatcher
 
 # source: https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression?page=1&tab=scoredesc#tab-top
 email_regex = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 
 # source: chatgpt
-phone_number_regex = r"""(?:\+?\d{1,3}[-.\s()]*)?(?:\(?\d{1,4}\)?[-.\s]*)?\d{1,4}[-.\s]*\d{1,4}[-.\s]*\d{1,9}"""
+# phone_number_regex = r"""(?:\+?\d{1,3}[-.\s()]*)?(?:\(?\d{1,4}\)?[-.\s]*)?\d{1,4}[-.\s]*\d{1,4}[-.\s]*\d{1,9}"""
+phone_number_regex = r"""(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d*)\)?)[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?)+)(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))"""
 
 # source: chatgpt
 ip_regex = r"""\b(?:\d{1,3}\.){3}\d{1,3}\b"""
@@ -21,9 +24,12 @@ def mask_emails(text: str) -> tuple[str, int]:
 
 
 def mask_phone_numbers(text: str) -> tuple[str, int]:
-    masked_text = phone_number_regex_compiled.sub("|||PHONE_NUMBER|||", text)
-    num_phone_numbers = len(phone_number_regex_compiled.findall(text))
-    return masked_text, num_phone_numbers
+    matches = PhoneNumberMatcher(text, "US", leniency=phonenumbers.Leniency.POSSIBLE)
+    count = 0
+    for match in matches:
+        count += 1
+        text = text.replace(match.raw_string, "|||PHONE_NUMBER|||")
+    return text, count
 
 
 def mask_ips(text: str) -> tuple[str, int]:
